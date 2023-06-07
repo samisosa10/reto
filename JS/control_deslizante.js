0241e04dcca8959359d93bde066d3c3da383b3b0
samuel giraldo <samueldavidgiraldogutierrez@gmail.com>
2023-06-07 17:07:34 -0500

    pequeño reto about trello

---
+++ JS/control_deslizante.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".control-deslize-container").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: true,
+      prevArrow: $(".testimonial-carousel-controls .prev"),
+      nextArrow: $(".testimonial-carousel-controls .next")
+    });
+  });
+  
+  
